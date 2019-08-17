1b90e2bf05ed99b7e15473f2c5ecb82ebcf9c8c5
Pavel <pavel.k.danilin@gmail.com>
2019-08-17 14:17:08 +1000

    Strong comparision operator

--- src/parser/node/expr.js
+++ src/parser/node/expr.js
@@ -37,12 +37,12 @@ export default class ASTNodeExpr extends ASTNode {
       case OPERATOR.MOD: return this.left.execute(scope) % this.right.execute(scope);
       case OPERATOR.GT: return this.left.execute(scope) > this.right.execute(scope);
       case OPERATOR.LT: return this.left.execute(scope) < this.right.execute(scope);
-      case OPERATOR.EQ: return this.left.execute(scope) == this.right.execute(scope);
+      case OPERATOR.EQ: return this.left.execute(scope) === this.right.execute(scope);
       case OPERATOR.GET: return this.left.execute(scope) >= this.right.execute(scope);
       case OPERATOR.LET: return this.left.execute(scope) <= this.right.execute(scope);
       case OPERATOR.AND: return this.left.execute(scope) && this.right.execute(scope);
       case OPERATOR.OR: return this.left.execute(scope) || this.right.execute(scope);
-      case OPERATOR.NEQ: return this.left.execute(scope) != this.right.execute(scope);
+      case OPERATOR.NEQ: return this.left.execute(scope) !== this.right.execute(scope);
       case OPERATOR.DOT: return this.right.execute(this.left.execute(scope));
       case OPERATOR.IN: return this.in(this.left.execute(scope), this.right.execute(scope));
       default:
